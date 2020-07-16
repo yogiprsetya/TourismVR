0fa95e5f72295b0bd4cf0bb1751de5808f8d78c3
Yogi Prasetya <yogiprsetya@gmail.com>
2020-07-16 22:47:20 +0700

    Section 4, Lesson 1: shared surface,  infospot implementation setup

--- client.js
+++ client.js
@@ -1,7 +1,4 @@
-// This file contains the boilerplate to execute your React app.
-// If you want to modify your application's content, start in "index.js"
-
-import {ReactInstance} from 'react-360-web';
+import {ReactInstance, Surface} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -10,14 +7,77 @@ function init(bundle, parent, options = {}) {
     ...options,
   });
 
-  // Render your app content to the default cylinder surface
-  r360.renderToSurface(
-    r360.createRoot('TourismVR', { /* initial props */ }),
-    r360.getDefaultSurface()
+  introPanel = new Surface(
+    500,
+    400,
+    Surface.SurfaceShape.Cylinder
   );
 
-  // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+  introRoot = r360.renderToSurface(
+    r360.createRoot('TourismAppVR', {}),
+    introPanel
+  );
+
+  marketPanel = new Surface(
+    100,
+    100,
+    Surface.SurfaceShape.Flat
+  )
+
+  marketPanel.setAngle(
+    0.2,
+    0
+  );
+
+  museumPanel = new Surface(
+    100,
+    100,
+    Surface.SurfaceShape.Flat
+  )
+
+  museumPanel.setAngle(
+    Math.PI / 2,
+    0
+  );
+
+  restaurantPanel = new Surface(
+    100,
+    100,
+    Surface.SurfaceShape.Flat
+  )
+
+  restaurantPanel.setAngle(
+    -Math.PI / 2,
+    0
+  );
+
+  shoppingPanel = new Surface(
+    100,
+    100,
+    Surface.SurfaceShape.Flat
+  );
+
+  shoppingPanel.setAngle(
+    3.6,
+    0
+  );
+
+  r360.renderToSurface(
+    r360.createRoot('InfoPanel', {}),
+    marketPanel
+  )
+
+  r360.renderToSurface(
+    r360.createRoot('InfoPanel', {}),
+    shoppingPanel
+  )
+
+  r360.renderToSurface(
+    r360.createRoot('InfoPanel', {}),
+    restaurantPanel
+  )
+
+  r360.compositor.setBackground(r360.getAssetURL('gdansk.jpg'));
 }
 
 window.React360 = {init};
--- index.js
+++ index.js
@@ -1,43 +1,65 @@
 import React from 'react';
 import {
   AppRegistry,
+  asset,
+  Image,
+  NativeModules,
   StyleSheet,
   Text,
   View,
+  VrButton
 } from 'react-360';
 
-export default class TourismVR extends React.Component {
+class InfoPanel extends React.Component {
+  state = {
+    img: {
+      name: 'info.png',
+      width: 100,
+      height: 100
+    }
+  }
+
+  render() {
+    const { img } = this.state;
+
+    return (
+      <View style={styles.displayPanel}>
+        <Image 
+          source={asset(img.name)}
+          style={{width: img.width, height: img.height}}
+        />
+      </View>
+    )
+  }
+}
+
+export default class TourismAppVR extends React.Component {
   render() {
     return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
-        </View>
+      <View>
       </View>
     );
   }
 };
 
 const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
+  displayPanel: {
+    width: 100,
+    height: 100,
+    flexDirection: 'column',
   },
-  greetingBox: {
+  attractionBox: {
     padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
+    backgroundColor: '#F7F7F7',
+    borderColor: '#C4002F',
     borderWidth: 2,
+    width: 500
   },
-  greeting: {
+  attractionText: {
     fontSize: 30,
+    color: '#C4002F'
   },
 });
 
-AppRegistry.registerComponent('TourismVR', () => TourismVR);
+AppRegistry.registerComponent('TourismAppVR', () => TourismAppVR);
+AppRegistry.registerComponent('InfoPanel', () => InfoPanel);
